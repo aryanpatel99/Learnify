500d2929424a10d246f5efc6853138757f5779d2
Aryan Patel <aryanpatel6215@gmail.com>
2025-07-21 23:36:22 +0530

    update: redirection to new prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['img.youtube.com'],
+  images: {
+    domains: ['img.youtube.com'],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://learnify-app-omega.vercel.app/', //  Replace with your desired URL
+        permanent: true, // true = 301 redirect; false = 302 redirect
       },
-    // reactStrictMode: false,
+    ];
+  },
 };
 
 export default nextConfig;
